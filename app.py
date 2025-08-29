e0e207c5d6b40f22614e1f722c38131657255f70
mttksng <yourfenrir@yandex.ru>
2025-08-30 02:06:47 +0300

    Добавлен метод get_home_page

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_homePage():
+    if request.method == 'GET':
+        return jsonify({"status": "ok"}), 200
+    else:
+        return 405
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=('cert.pem', 'key.pem'), debug=True, port=443)
