fc5b3b4f4c13bf018ea90cd01f8b3a13e746663f
mttksng0 <yourfenrir@yandex.ru>
2025-08-31 20:34:07 +0300

    QASRV-5 Добавлен end-point /user/<int:id>

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+users = [{"id": 1, "first_name": "Baizhi"},
+         {"id": 2, "first_name": "Chixia"}]
+
 
 @app.route('/', methods=['GET'])
 def get_home_page():
@@ -8,6 +11,15 @@ def get_home_page():
         return jsonify({"status": "ok"}), 200
     else:
         return 405
+
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_info(id):
+    user = next((user for user in users if user['id'] == id), None)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 
 if __name__ == "__main__":
